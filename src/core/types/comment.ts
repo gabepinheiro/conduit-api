61eab869d1f698c1de67530a8286c00cfb5f3f9e
Gabriel Pinheiro <gabrielpsilvas@live.com>
2023-03-06 17:31:45 -0300

    refactor(core/types): transform types using io-ts

--- src/core/types/comment.ts
+++ src/core/types/comment.ts
@@ -1,9 +1,13 @@
-import { Profile } from '@/core/types/profile'
+import { profileCodec } from '@/core/types/profile'
 
-export type Comment = {
-  id: number
-  createdAt: string
-  updatedAt: string
-  body: string
-  author: Profile
-}
+import * as t from 'io-ts'
+
+export const commentCodec = t.type({
+  id: t.number,
+  createdAtL: t.string,
+  updatedAt: t.string,
+  body: t.string,
+  authpr: profileCodec,
+})
+
+export type Comment = t.TypeOf<typeof commentCodec>
